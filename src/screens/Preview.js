b3a9f835c014384c2bedafdfd972a23560c4e249
kevando <kevin@frameri.com>
2017-03-06 02:51:50 -0500

    it was a fucking issue with the debugger -_-

--- src/screens/Preview.js
+++ src/screens/Preview.js
@@ -1,24 +1,36 @@
 import React, { Component } from 'react';
-import { Image, StyleSheet, View } from 'react-native';
+import { Image, StyleSheet, View, CameraRoll } from 'react-native';
 import { Container, Header, Title, ListItem, Content, Button, Text } from 'native-base';
 import { Actions } from 'react-native-router-flux';
-import { takeSnapshot, dirs } from "react-native-view-shot";
+import { takeSnapshot } from "react-native-view-shot";
 // cross platform dirs:
 // const { CacheDir, DocumentDir, MainBundleDir, MovieDir, MusicDir, PictureDir } = dirs;
 
 export default class Preview extends Component {
 
   onExportPress(){
-    console.log('export press',this.refs)
-//     takeSnapshot(this.refs["viewContainer"], {
-//   format: "jpeg",
-//   quality: 0.8
-// })
-// .then(
-//   uri => console.log("Image saved to", uri),
-//   error => console.error("Oops, snapshot failed", error)
-// );
+    // console.log('export press',this.refs)
+    var This = this;
+    takeSnapshot(this.refs["imageContainer"], {
+      format: "jpeg",
+      quality: 0.8
+    })
+    .then(
+      uri => This.savePhoto(uri),
+      error => console.error("Oops, snapshot failed", error)
+    );
+  }
 
+  savePhoto(uri) {
+
+    CameraRoll.saveToCameraRoll(uri, 'photo', function(data) {
+      // console.log(data);
+      // this doesnt work but whatever
+      alert('photo saved to camera roll')
+  }, function(err) {
+      // console.log(err);
+      alert('ERROR saving to camera roll')
+  });
   }
 
       render() {
@@ -26,7 +38,7 @@ export default class Preview extends Component {
 
             <View ref="viewContainer" style={{flex:1,backgroundColor:'yellow', padding:10,marginTop:80}}>
 
-              <Image source={require('../images/bg1.png')} style={styles.container} >
+              <Image ref="imageContainer" source={require('../images/bg1.png')} style={styles.container} >
                 <View style={styles.setlistContainer} >
                     <Text>Dude?</Text>
                     </View>
