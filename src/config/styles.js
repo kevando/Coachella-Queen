e139d454bab4f0f1dce270ac13304b65d6185972
kevando <kevin@frameri.com>
2017-03-11 04:20:33 -0500

    demo of animated welcome page

--- src/config/styles.js
+++ src/config/styles.js
@@ -1,5 +1,17 @@
 
 // ---------------------------------------
+// Coachella coolors baby
+// ---------------------------------------
+export const colors = {
+
+  orange: 'orange',
+  blue: '#10a6ed',
+  white: '#ffffff',
+  yellow: '#ffe60c',
+  green: '#24bd3a',
+}
+
+// ---------------------------------------
 // Router Styling
 // ---------------------------------------
 export const routerStyles = {
