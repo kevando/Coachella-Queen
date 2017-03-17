65ca13df574c5832222fe42e609c6747f0ad01a0
kevando <kevin@frameri.com>
2017-03-17 15:01:49 -0400

    tried to add screenshot detection, currently not working

--- index.ios.js
+++ index.ios.js
@@ -5,8 +5,48 @@ console.ignoredYellowBox = ['Remote debugger is in a background tab which may ca
 
 AppRegistry.registerComponent('CoachellaQueen', () => App);
 
+// ================================================================
+// Attempt to get screenshot detect working
+// ================================================================
 
-// Snapshot Example code
-// import { AppRegistry } from "react-native";
-// import App from "./App";
-// AppRegistry.registerComponent("CoachellaQueen", () => App);
+// import React, { Component } from 'react';
+// import { AppRegistry, Text, View, NativeEventEmitter } from 'react-native';
+//
+// var RNScreenshotDetector = require('react-native-screenshot-detector');
+//
+// class HelloWorldApp extends Component {
+//
+//   componentDidMount() {
+//     console.log('=============== RNScreenshotDetector =================');
+//
+//     // RNScreenshotDetector
+//
+//     var emitter = RNScreenshotDetector.subscribe(function(){
+//       // alert('screenshot taken!')
+//       console.log('SUBSCRIBE');
+//     });
+//
+//     // console.log('emitter',emitter)
+//
+//     // const eventEmitter = new NativeEventEmitter(RNScreenshotDetector);
+//     emitter.addListener(RNScreenshotDetector.SCREENSHOT_EVENT, this.someCallback, {});
+//
+//   }
+//
+//   someCallback() {
+//     console.log('some callback')
+//   }
+//
+//   render() {
+//     return (
+//       <View style={{paddingTop:100}} >
+//         <Text>Hello world!</Text>
+//         <Text>dude</Text>
+//         <Text>{RNScreenshotDetector.SCREENSHOT_EVENT}</Text>
+//         <Text>duuuuuuuuuude</Text>
+//       </View>
+//     );
+//   }
+// }
+//
+// AppRegistry.registerComponent('CoachellaQueen', () => HelloWorldApp);
